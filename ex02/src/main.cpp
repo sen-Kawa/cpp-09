8c29bc74a5e4b328ffd5a06737d071f1df63cd9d
olin_tp_ar <universalexperimental@protonmail.com>
2023-06-11 22:33:22 +0200

    main checking for the args

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -1,8 +1,13 @@
 #include "../header/PmergeMe.class.hpp"
 #include "../header/colours.hpp"
 
-int main(void)
+int main(int argc, char **argv)
 {
+	if (argc != 2)
+	{
+		std::cout << RED << "Error: invalid arguments." << std::endl;
+		exit(-1);
+	}
 	return (0);
 }
 
